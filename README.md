1c724d5ba1427a977a30fe48dfc1368ed97f0009
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2018-05-04 16:02:29 +0100

    Initial definitions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CloudBiofoundry
 Cloud Biofoundry
+
+## Steps
+
+1. Biosynthetic pathway from chassis to target computed through **RetroPath** (retrosynthesis).
+1. Enzyme sequences selected for each reaction step through **Selenzyme**.
+1. Assign kinetic constants ranges to each sequence.
+1. Specify experimental parameters.
+1. Generate experimental design.
+1. For each construct, generate SBML dynamic model of the pathway.
+1. Simulate pathways through ensemble modeling.
---
+++ path2doe.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Doe (c) University of Manchester 2018
+
+Doe is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+Created on Fri May  4 13:50:43 2018
+
+@author: Pablo Carbonell
+@description: SYNBIOCHEM Path to DoE
+"""
+import pandas as pd
+
+
+def readPathway(pFile):
+    """
+    Read the pathway provided by RetroPath
+    """
+    pass
+
+def readEnzymes(pFile):
+    """
+    Read the list of enzymes from Selenzyme 
+    """
+    pass
+
+def readKinetics():
+    """
+    For each enzyme, annotate kinetics
+    """
+    pass
+
+def doeSpec():
+    """
+    Basic DoE specifications:
+        - Number of plasmids
+        - Number of promoter
+        - Gene shuffling
+    """
+    pass
+
+def writeDoeFile():
+    """
+    Prepare the input file for the DoE software
+    """
+    pass
+
+def runDoe():
+    """
+    """
+    pass
+
+def makePathways():
+    """
+    Generate SBML models for each construct 
+    """
+    pass
